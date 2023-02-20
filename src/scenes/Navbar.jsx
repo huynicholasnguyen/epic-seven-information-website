512f5c27783f25b1d7cc2547bf24e32e9ff243cf
huynicholasnguyen <hnguyen62048@gmail.com>
2023-02-20 16:07:39 -0500

    useMediaQuery

--- src/scenes/Navbar.jsx
+++ src/scenes/Navbar.jsx
@@ -5,7 +5,7 @@ import useMediaQuery from "../hooks/useMediaQuery";
 const Link = ({ page, selectedPage, setSelectedpage}) => {
     const lowerCasePage = page.toLowerCase();
     return (
-        <AnchorLink className = {`${selectedPage === lowerCasePage ? "text-yellow" : "text-white"}
+        <AnchorLink className = {`${selectedPage === lowerCasePage ? "text-yellow" : ""}
         hover: text-yellow transition duration-500`}
         href = {`#${lowerCasePage}`}
         onClick = {() => setSelectedpage(lowerCasePage)}
@@ -23,6 +23,7 @@ const Navbar = ({selectedPage, setSelectedpage}) => {
         <nav className = {`z-40 w-full fixed top-0 py-6`}>
             <div className = "flex items-center justify-between mx-auto w-5/6">
                 <h4 className = "font-playfair text-3xl font-bold">Epic Seven</h4>
+                <h4 className = 'font-opensans text-4xl font-bold'>HELLO WORLD</h4>
 
                 {/*DESKTOP NAV */}
                 {isAboveSmallScreens ? (
@@ -32,9 +33,14 @@ const Navbar = ({selectedPage, setSelectedpage}) => {
                             selectedPage = {selectedPage}
                             setSelectedPage = {setSelectedpage}
                         />
+                        <Link 
+                            page = "Home"
+                            selectedPage = {selectedPage}
+                            setSelectedPage = {setSelectedpage}
+                        />
                     </div>
                 ) : (
-                <div></div>
+                <div>Test</div>
                 ) }
             </div>
         </nav>
