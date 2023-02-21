0c97a85e6d6cea8a353fcf8e84c388166c1f5436
huynicholasnguyen <hnguyen62048@gmail.com>
2023-02-21 00:31:25 -0500

    Changed isAboveSmallScreens to isDesktop and fixed conditions

--- src/scenes/Navbar.jsx
+++ src/scenes/Navbar.jsx
@@ -17,7 +17,7 @@ const Link = ({ page, selectedPage, setSelectedpage}) => {
 
 const Navbar = ({selectedPage, setSelectedpage}) => {
     const [isMenuToggled, setisMenuToggled] = useState(false);
-    const isAboveSmallScreens = useMediaQuery("(min-width: 768 px)");
+    const isDesktop = useMediaQuery("(min-width: 768px)");
 
     return(
         <nav className = {`z-40 w-full fixed top-0 py-6`}>
@@ -25,41 +25,32 @@ const Navbar = ({selectedPage, setSelectedpage}) => {
                 <h4 className = "font-playfair text-3xl font-bold">Epic Seven</h4>
                 
                 {/*DESKTOP NAV */}
-                {isAboveSmallScreens ? (
+                {isDesktop ? (
                     <div className = "flex justify-between gap-16 font-opensans text-sm font-semibold">
                         <Link 
-                            page = "Home"
-                            selectedPage = {selectedPage}
-                            setSelectedPage = {setSelectedpage}
-                        />
-                        <Link 
-                            page = "Home"
-                            selectedPage = {selectedPage}
-                            setSelectedPage = {setSelectedpage}
-                        />
-                    </div>
-                ) : (
-                <div className = "flex justify-between gap-16 font-opensans text-sm font-semibold">
-                    <Link 
                         page = "Home"
                         selectedPage = {selectedPage}
                         setSelectedPage = {setSelectedpage}
+                        />
+                        <Link 
+                            page = "Stats"
+                            selectedPage = {selectedPage}
+                            setSelectedPage = {setSelectedpage}
+                        />
+                        <Link 
+                            page = "Catalysts"
+                            selectedPage = {selectedPage}
+                            setSelectedPage = {setSelectedpage}
+                        />
+                        <Link 
+                            page = "RTA Drafts"
+                            selectedPage = {selectedPage}
+                            setSelectedPage = {setSelectedpage}
                     />
-                    <Link 
-                        page = "Stats"
-                        selectedPage = {selectedPage}
-                        setSelectedPage = {setSelectedpage}
-                    />
-                    <Link 
-                        page = "Catalysts"
-                        selectedPage = {selectedPage}
-                        setSelectedPage = {setSelectedpage}
-                    />
-                    <Link 
-                        page = "RTA Drafts"
-                        selectedPage = {selectedPage}
-                        setSelectedPage = {setSelectedpage}
-                    />
+                    </div>
+                ) : (
+                <div>
+                    
                 </div>
                 ) }
             </div>
