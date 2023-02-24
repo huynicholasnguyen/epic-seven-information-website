6c6bcf8fcb189e71c10734e60d80e97fec7d2120
huynicholasnguyen <hnguyen62048@gmail.com>
2023-02-24 00:38:39 -0500

    Created Menu for Mobile, adding elements to the menu. Will add images to buttons later.

--- src/scenes/Navbar.jsx
+++ src/scenes/Navbar.jsx
@@ -49,10 +49,46 @@ const Navbar = ({selectedPage, setSelectedpage}) => {
                     />
                     </div>
                 ) : (
-                <div>
+                <button className = "rounded-full bg-red p-2" onClick = {() => setisMenuToggled(!isMenuToggled)}>
                     
-                </div>
+                </button>
                 ) }
+
+                {/*MOBILE*/}
+                {!isDesktop && isMenuToggled && (
+                    <div className = "fixed right-0 bottom-0 h-full bg-blue w-[300px]">
+                        {/* CLOSE ICON */}
+                        <div className = "flex justify-end p-12">
+                            <button onClick = {() => setisMenuToggled(!isMenuToggled)}>
+
+                            </button>
+                        </div>
+
+                        {/* MENU ITEMS */}
+                        <div className = "flex flex-col gap-10 ml-[33%] text-2xl text-deep-blue">
+                        <Link 
+                        page = "Home"
+                        selectedPage = {selectedPage}
+                        setSelectedPage = {setSelectedpage}
+                        />
+                        <Link 
+                            page = "Stats"
+                            selectedPage = {selectedPage}
+                            setSelectedPage = {setSelectedpage}
+                        />
+                        <Link 
+                            page = "Catalysts"
+                            selectedPage = {selectedPage}
+                            setSelectedPage = {setSelectedpage}
+                        />
+                        <Link 
+                            page = "RTA Drafts"
+                            selectedPage = {selectedPage}
+                            setSelectedPage = {setSelectedpage}
+                        />
+                        </div>
+                    </div>
+                )}
             </div>
         </nav>
     )
