bf194b86cc3ddf6c53a88e8e50bea548644a494e
huynicholasnguyen <hnguyen62048@gmail.com>
2023-03-08 03:09:04 -0500

    Added some test images for landing page

--- src/scenes/Landing.jsx
+++ src/scenes/Landing.jsx
@@ -9,13 +9,13 @@ const Landing = ({setSelectedPage}) => {
         <section id = "home" className = "md:flex md:justify-between md:items-center md:h-full gap-16 py-10">
 
             {/* IMAGE SECTION */}
-            <div className = "md:order-2 flex justify-center basis-3/5 z-10 mt-16 md:mt-32">
+            <div className = "basis-3/5 z-10 mt-16 md:mt-32 flex justify-center md:order-2">
                 {isAboveMediumScreens ? (
                     <div className = "relative z-0 ml-20 before:absolute before:-top-20 before:-left-20 before:rounded-t-[400px] before:w-full before:max-w-[400px] md:before:max-w-[600px] before:h-full before:border-2 before:border-blue before:z-[-1]">
                         <img
                             alt = "lcbellona-art"
                             className="hover:filter hover:saturate-200 transition duration-500 z-10 w-full max-w-[400px] md:max-w-[600px]"
-                            src = "assets/lcbellona-art.png"
+                            src = "/assets/lcbellona-art.png"
                         />
                     </div>
                 ) : (
@@ -23,7 +23,7 @@ const Landing = ({setSelectedPage}) => {
                         <img
                             alt = "lcbellona-art"
                             className="hover:filter hover:saturate-200 transition duration-500 z-10 w-full max-w-[400px] md:max-w-[600px]"
-                            src = "assets/lcbellona-art.png"
+                            src = "/assets/lcbellona-art.png"
                         />
                     </div>
                 )}
