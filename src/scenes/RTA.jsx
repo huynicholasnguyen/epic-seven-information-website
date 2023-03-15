22045c96552783fead17f8bf44ad0f81bc7e352d
huynicholasnguyen <hnguyen62048@gmail.com>
2023-03-15 15:46:33 -0400

    Update RTA.jsx

--- src/scenes/RTA.jsx
+++ src/scenes/RTA.jsx
@@ -1,6 +1,38 @@
 import LineGradient from "../components/LineGradient";
 import { motion } from 'framer-motion';
 
+const container = {
+    hidden: {},
+    visible: {
+        transition: { staggerChildren: 0.2}
+    }
+}
+
+const rtaVariant = {
+    hidden: {opacity: 0, scale: 0.8},
+    visible: {opacity: 1, scale: 1}
+}
+
+const Draft = ({title}) => {
+    const overlayStyles = `absolute h-full w-full opacity-0 hover: opactiy-90 transition duration-500 bg-grey z-30 flex flex-col justify-center items-center text-center p-16 text-deep-blue`;
+    const draftTitle = title.split(" ").join("-").toLowerCase();
+
+    return (
+        <motion.div
+            variants = {rtaVariant} className = 'relative'
+        >
+            <div className = {overlayStyles}>
+                <p className = 'text-2xl font-playfair'>
+                    {title}
+                </p>
+                <p>
+                    Insert text here.
+                </p>
+            </div>
+        </motion.div>
+    )
+}
+
 const RTA = () => {
     return (
         <section id='projects' className='pt:48 pb-48'>
@@ -30,6 +62,26 @@ const RTA = () => {
                 </div>
 
             </motion.div>
+
+            {/*RTA information*/}
+
+            <div className='flex justify-center'>
+                <motion.div
+                    className="sm:grid sm:grid-cols-3"
+                    initial="hidden"
+                    whileInView="visible"
+                    viewport={{ once: true, amount: 0.5 }}
+                    variants={container}
+                >
+                    <div
+                        className = 'flex justify-center text-center items-center p-10 bg-black max-w-[400px] max-h-[400px] text-2xl font-playfair font-semibold'
+                    >
+                        Some text here
+                    </div>
+                    <Draft title = "Draft 1"/>
+                    
+                </motion.div>
+            </div>
         </section>
     )
 }
